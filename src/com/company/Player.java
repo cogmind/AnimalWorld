521b05024d75b5a3ebeeb6c9b6e267336f45e66e
cogmind <daniel@labbe.de>
2021-01-12 13:26:31 +0100

    Embellished Player with initial money etc.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,7 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Player {
-    String name;
-    int money;
+
+    private String name;
+    private int money;
+    private static final int INITIAL_MONEY = 1000;
+    private ArrayList<Animal> animals = new ArrayList<>();
+
+    public Player(String name){
+        this.setName(name);
+        this.setMoney(INITIAL_MONEY);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Private because name changes are not allowed.
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 
 }
