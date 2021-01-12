43907ee619920a4f07f8e82722948fc35213e36f
cogmind <daniel@labbe.de>
2021-01-12 15:05:26 +0100

    Updated Game class and added displayMoney and average health functionality to the View

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,39 +6,84 @@ public class Game {
 
     private View view = new View();
     private boolean gameOver = false;
+
     public final static byte MIN_ROUNDS = 5;
     public final static byte MAX_ROUNDS = 30;
     public final static byte MIN_PLAYERS = 1;
     public final static byte MAX_PLAYERS = 4;
+    public final static byte BREEDING_SUCCESS_RATE = 50;
 
     public Game() {
 
-        while (!gameOver) {
+        Scanner scanner = new Scanner(System.in);
+        byte rounds = -1;
+        byte players = -1;
 
-            Scanner scanner = new Scanner(System.in);
-            byte rounds = -1;
-            byte players = -1;
+        while (!(rounds >= MIN_ROUNDS && rounds <= MAX_ROUNDS)) {
+            view.howManyRounds();
+            rounds = scanner.nextByte();
 
-            while (!(rounds >= MIN_ROUNDS && rounds <= MAX_ROUNDS)) {
-                view.howManyRounds();
-                rounds = scanner.nextByte();
-
-                if (rounds < 5 || rounds > 30) {
-                    view.roundsOutOfBounds();
-                }
+            if (rounds < 5 || rounds > 30) {
+                view.roundsOutOfBounds();
             }
+        }
 
-            while (!(players >= MIN_PLAYERS && players <= MAX_PLAYERS)) {
-                view.howManyPlayers();
-                players = scanner.nextByte();
+        while (!(players >= MIN_PLAYERS && players <= MAX_PLAYERS)) {
+            view.howManyPlayers();
+            players = scanner.nextByte();
 
-                if (players < MIN_PLAYERS || players > MAX_PLAYERS) {
-                    view.playersOutOfBounds();
-                }
+            if (players < MIN_PLAYERS || players > MAX_PLAYERS) {
+                view.playersOutOfBounds();
             }
+        }
+
+        byte menuChoice;
+        // TODO for each player
+        // TODO If all players and rounds have been consumed, set gameOver to true OR break
+        while (!gameOver && rounds > 0) {
 
             view.displayMainMenu();
+            menuChoice = scanner.nextByte();
 
+            final byte MENU_START = 1;
+            final byte MENU_END = 5;
+
+            if (menuChoice < MENU_START || menuChoice > MENU_END) {
+                view.pleaseSelect();
+            } else {
+
+                switch (menuChoice) {
+                    case 1 -> buyAnimal();
+                    case 2 -> buyFood();
+                    case 3 -> feed();
+                    case 4 -> breed();
+                    case 5 -> sell();
+                }
+
+            }
+            rounds--;
         }
+        view.endOfGame();
+    }
+
+    public void buyAnimal() {
+        view.displayBuyAnimalMenu();
+
+    }
+
+    public void buyFood() {
+        view.displayBuyFoodMenu();
+    }
+
+    public void feed() {
+        view.displayFeedMenu();
+    }
+
+    public void breed() {
+        view.displayBreedMenu();
+    }
+
+    public void sell() {
+        view.displaySellAnimalMenu();
     }
 }
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -1,48 +1,93 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class View {
 
     //TODO Gather all Sysouts here
-    public View(){
+    public View() {
 
     }
 
-    private void print(String input){
+    private void print(String input) {
         System.out.println("\n".repeat(60));
         System.out.println(input);
     }
+
+    private void error(String input){
+        System.err.println(input);
+    }
+
     public void lineFeed() {
         print("\n");
     }
 
+    public void displayMoney(long money){
+        System.out.print(money + "€\t");
+    }
+
+    public void displayAverageHealth(ArrayList<Integer> health){
+        // TODO Compute average health
+        int HP_TOTAL = 0;
+        int length = 0;
+        for(int hp : health) {
+            HP_TOTAL += hp;
+            length += 1;
+        }
+        System.out.printf("Total HP: %f Avg HP: %d", ((HP_TOTAL + 0.0) / length), HP_TOTAL);
+    }
+
     public void howManyRounds() {
-        print("How many rounds (" + Game.MIN_ROUNDS + "-" + Game.MAX_ROUNDS + ") would you like to play?");
+        System.out.println("How many rounds (" + Game.MIN_ROUNDS + "-" + Game.MAX_ROUNDS + ") would you like to play?");
     }
 
 
-    public void roundsOutOfBounds(){
-        print("Rounds must be between " + Game.MIN_ROUNDS + " and " + Game.MAX_ROUNDS + ". Try again");
+    public void roundsOutOfBounds() {
+        error("Rounds must be between " + Game.MIN_ROUNDS + " and " + Game.MAX_ROUNDS + ". Try again");
     }
 
-    public void howManyPlayers(){
+    public void howManyPlayers() {
         print("How many players ("+ Game.MIN_PLAYERS + "-" + Game.MAX_PLAYERS + ")?");
     }
 
-    public void playersOutOfBounds(){
-        print("The number of players has to be between " + Game.MIN_PLAYERS + " and " + Game.MAX_PLAYERS + ".");
+    public void playersOutOfBounds() {
+        error("The number of players has to be between " + Game.MIN_PLAYERS + " and " + Game.MAX_PLAYERS + ".");
     }
 
     public void displayMainMenu() {
-        print("Options\n1. Buy animals\n2. Feed animals\n3. Skip round");
+        System.out.println("OPTIONS (Select one)\n1. Buy animals\n2. Buy food \n3. Feed animals\n4. Breed new animals (success rate " + Game.BREEDING_SUCCESS_RATE + "%) \n5. Sell animals\n");
     }
 
-    public void displayAnimalMenu(){
+    public void pleaseSelect(){
+        error("Please select an option between 1 and 5.");
+    }
+
+    public void displayBuyAnimalMenu() {
         //TODO animal store menu
         print("TMP Store menu goes here");
     }
 
-    public void displayFeedMenu(){
-        //TODO (animal) feed store menu
+    public void displayBuyFoodMenu() {
+        //TODO food store menu
+        print("TMP Food menu goes here");
+    }
+
+    public void displayFeedMenu() {
+        //TODO feeding menu
         print("TMP Feed menu goes here");
+    }
+
+    public void displayBreedMenu() {
+        print("TMP Breeding menu goes here");
+
+    }
+
+    public void displaySellAnimalMenu() {
+        print("TMP Sell animal menu goes here");
+    }
+
+    public void endOfGame() {
+        System.out.println("End of game. Exiting...");
+        System.exit(0);
     }
 }
